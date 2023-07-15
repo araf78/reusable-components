7938a9990af89b842e44debed754a4500020b530
araf78 <alarafath71@gmail.com>
2023-07-15 18:01:07 +0600

    remove something

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import Banner from './components/banner/Banner';
 
 function App() {
   return (
@@ -7,7 +6,6 @@ function App() {
      <h1 className="text-3xl bg-green-800 font-bold underline">
       Hello world!
     </h1>
-      <Banner color='error'>New brand identity has been launched for the <a href="/" className="inline font-medium text-blue-600 underline underline-offset-2 decoration-600 decoration-solid hover:no-underline">Contact Us</a></Banner>
     </div>
   );
 }
