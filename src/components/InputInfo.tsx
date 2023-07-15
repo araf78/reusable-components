0f1ce43073467297a49ebbae0e94479dcae57797
Rabiul Hossen <rhsimon69@gmail.com>
2023-07-15 13:31:33 +0600

    input comp for form page

---
+++ src/components/InputInfo.tsx
@@ -0,0 +1,67 @@
+// import React from 'react';
+
+// interface InputProps {
+//    name: String;
+//    label: String;
+//    type: String;
+//    value: String;
+//    placeholder: String;
+//    className: String;
+//    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+// }
+
+// const Input = ({
+//    name,
+//    label,
+//    type,
+//    value,
+//    placeholder,
+//    className,
+//    onChange,
+// }: InputProps) => {
+//    return (
+//       <div className={className}>
+//          <label htmlFor={name}> {label}</label>
+//          <input type={type}  placeholder={placeholder}value={value} onChange={onchange}/>
+//       </div>
+//    );
+// };
+
+
+
+import React from "react";
+
+interface InputProps {
+  name: string;
+  label: string;
+  type: string;
+  value: string;
+  placeholder: string;
+  className: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  name,
+  label,
+  type,
+  value,
+  placeholder,
+  className,
+  onChange,
+}: InputProps) => {
+  return (
+    <div className={className}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
+
