a3edf0e8d8da0b42e60c6f44e04045a99aeae746
araf78 <alarafath71@gmail.com>
2023-07-15 16:42:49 +0600

    banner updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css';
 import Banner from './components/banner/Banner';
-import Collapse from './components/collapse/Collapse';
-import ToggleSwitch from './components/switch';
 
 function App() {
   return (
@@ -9,7 +7,7 @@ function App() {
      <h1 className="text-3xl bg-green-800 font-bold underline">
       Hello world!
     </h1>
-      <Banner type='success'>New brand identity has been launched for the <a href="https://flowbite.com" className="inline font-medium text-blue-600 underline dark:text-blue-500 underline-offset-2 decoration-600 dark:decoration-500 decoration-solid hover:no-underline">Contact Us</a></Banner>
+      <Banner bgColor='success' textColor='success'>New brand identity has been launched for the <a href="/" className="inline font-medium text-blue-600 underline underline-offset-2 decoration-600 decoration-solid hover:no-underline">Contact Us</a></Banner>
     </div>
   );
 }
--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,32 +3,36 @@ import { HiOutlineLightBulb } from 'react-icons/hi';
 
 type BannerProps = {
   children: ReactNode;
-  type?: 'success' | 'warning' | 'error';
+  // type?: 'success' | 'warning' | 'error';
+  bgColor?: 'primary' | 'success',
+  borderColor?: 'primary' | 'success',
+  textColor?: 'primary' | 'success'
 };
 
-const Banner: React.FC<BannerProps> = ({ children, type = 'success' }) => {
+const Banner: React.FC<BannerProps> = ({ children, textColor='success', bgColor='success', borderColor='success' }) => {
   const [isClosed, setIsClosed] = useState(false);
 
-  let bgColor = 'bg-gray-50';
-  let borderColor = 'border-gray-200';
-  let textColor = 'text-gray-400';
-
-  if (type === 'warning') {
-    bgColor = 'bg-yellow-500';
-    borderColor = 'border-yellow-600';
-  } else if (type === 'error') {
-    bgColor = 'bg-red-500';
-    borderColor = 'border-red-600';
+  const bgColors = {
+    primary: 'bg-gray-50 border-gray-200',
+    success: 'bg-yellow-500 border-yellow-600',
+  }
+  const textColors = {
+    primary: 'text-gray-400',
+    success: 'text-yellow-600',
   }
 
   const handleClose = () => {
     setIsClosed(true);
   };
 
+  if (isClosed) {
+    return null;
+  }
+
   return (
-    !isClosed ? (<div id="sticky-banner" className={`fixed top-0 left-0 z-50 flex justify-between w-full p-4 border-b ${bgColor} ${borderColor} `}>
+    <div id="sticky-banner" className={`fixed top-0 left-0 z-50 flex justify-between w-full p-4 border-b ${bgColors[bgColor]} `}>
       <div className="flex items-center mx-auto">
-        <p className="flex items-center text-sm font-normal text-gray-500 ">
+        <p className={`flex items-center text-sm font-normal ${textColor}`}>
           <span className="inline-flex p-1 mr-3 bg-gray-200 rounded-full w-6 h-6 items-center justify-center">
             <HiOutlineLightBulb />
             <span className="sr-only">Light bulb</span>
@@ -37,15 +41,13 @@ const Banner: React.FC<BannerProps> = ({ children, type = 'success' }) => {
         </p>
       </div>
       <div className="flex items-center">
-        <button onClick={handleClose} data-dismiss-target="#sticky-banner" type="button" className={`flex-shrink-0 inline-flex justify-center w-7 h-7 items-center ${textColor} rounded-lg text-sm p-1.5`}>
+        <button onClick={handleClose} data-dismiss-target="#sticky-banner" type="button" className={`flex-shrink-0 inline-flex justify-center w-7 h-7 items-center ${textColors[textColor]} rounded-lg text-sm p-1.5`}>
           <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
           </svg>
         </button>
       </div>
     </div>
-
-    ) : null
   );
 };
 
